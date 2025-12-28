4815980a9fee7fe2661247f56280bdb887a3ae08
lollodelga <lollodg22@gmail.com>
2025-12-28 17:50:33 +0100

    Controllo errori

--- src/main/java/ldg/progettoispw/viewcli/Printer.java
+++ src/main/java/ldg/progettoispw/viewcli/Printer.java
@@ -8,7 +8,6 @@ public class Printer {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_CYAN = "\u001B[36m";
 
     private Printer() {}
 
